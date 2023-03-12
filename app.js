8abb8b850e3f00e3a3ff52612d0041412f684e22
Akarsh-KN <akarshkn136259@gmail.com>
2023-03-12 19:35:45 +0530

    Level 5 - Use of session authentication

--- app.js
+++ app.js
@@ -1,151 +1,150 @@
 //jshint esversion:6
 
 require("dotenv").config();
-const express = require("express");
+const express=require("express")
+const app=express()
+const ejs=require("ejs")
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
-const sha256 = require("sha256");
-const bcrypt = require("bcrypt");
+const bcrypt=require("bcrypt")
+const session=require("express-session")
+const passport=require("passport")
 const saltRounds = 10;
+const LocalStrategy=require("passport-local").Strategy;
+app.use(express.urlencoded({extended:false}))  //Not using bodyParser, using Express in-built body parser instead
+app.set("view engine","ejs")
+app.use(express.static("public"))
 
 
-const app = express();
+app.use(session({
+    secret:"Justarandomstring.",
+    resave:false,
+    saveUninitialized:false
+}))
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const urlMongodb = "mongodb+srv://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@cluster0.rejngoy.mongodb.net/usersDB?retryWrites=true&w=majority"
+const urlMongodb = "mongodb+srv://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@cluster0.rejngoy.mongodb.net/usersDB2?retryWrites=true&w=majority"
 
-// mongoose.connect(urlMongodb, 
-//     {useNewUrlParser: true}).then(() => {
-//         console.log("Connected to MongoDB");
-//     }).catch((err) => {
-//         console.log("Mongo DB connection failed");
-//     });
+mongoose.connect(urlMongodb, 
+        {useNewUrlParser: true}).then(() => {
+            console.log("Connected to MongoDB");
+        }).catch((err) => {
+            console.log("Mongo DB connection failed");
+        });
 
+const userSchema= new mongoose.Schema({
+    username : String,
+    password : String
+})
 
-var promise = mongoose.connect(urlMongodb, {useNewUrlParser: true});
+const User=new mongoose.model("User",userSchema)
 
-promise.then( () => {
-   console.log("Connected to MongoDB");
-});
+//Creating Local Strategy. passport-local-mongoose 3 lines of code for Strategy, 
+//Serialiazation, Deserialization not working due to recent changes in Mongoose 7
 
-const userSchema = new mongoose.Schema ({
-    email: String,
-    password: String
-});
-
-
-// Line responsible for encrypting the passwords.
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
-
-
-
-const User = new mongoose.model("User", userSchema);
-
-
-app.get("/home", function(req, res){
-    res.render("home");
-});
-
-
-
-/////login section both get and poist
-
-app.get("/login", function(req, res){
-    res.render("login");
-});
-
-app.post("/login", async function(req,res){
+passport.use(new LocalStrategy((username,password,done)=>{  //done is a callback function
     try{
-        const foundUser = await User.find({"email": req.body.username}).exec();
-
-        if (foundUser[0] == null){
-            console.log("The user doesnot exist");
-            res.render("login");
-        }
-        else{
-            if (foundUser[0].email){
-                
-                bcrypt.compare(req.body.password, foundUser[0].password, function(err, result) {
-
-                    if (result == true){
-                        res.render("secrets");
-                    }
-                    else{
-                        console.log("the password entered is wrong");
-                    }
-                });
+        User.findOne({username:username}).then(user=>{
+            if (!user){
+                return done(null,false, {message:"Incorrect Username"})
             }
-            else{
-                console.log("The user email is wrong");
-            };
-        };
+ //using bcrypt to encrypt passoword in register post route and compare function in login post round. 
+ //login post route will check here during authentication so need to use compare here  
+            bcrypt.compare(password,user.password,function(err,result){ 
+                if (err){
+                    return done(err)
+                }
 
+                if (result) {
+                    return done(null,user)
+                }
+                else {
+                    return done (null,false, {message:"Incorrect Password"})
+                }
+            })
+
+        })
     }
     catch (err){
-        console.log(err);
-        console.log("there is some error");
-    };
+            return done(err)
+    }
 
-});
-
-
-
-/// registering section 
-
-app.get("/register", function(req, res){
-    res.render("register");
-});
-
-
-app.post("/register", async function(req, res){
-    try{
-        bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
-
-            if (err) {
-                console.log(err);
-            }
-            else{
-                const newUser = new User({
-                    email : req.body.username,
-                    password: hash
-                });
-    
-                const doc = await User.find({"email": req.body.username}).exec();
-    
-                if (doc[0] == null){
-    
-                    newUser.save().then(() => {
-                        console.log("User created successfully")
-                        res.render("secrets");
-                        }).catch((err) => {
-                            console.log(err);
-                        });
-                    }
-                    else{
-                        console.log(doc);
-                        console.log("the user already exist");
-                        res.render("home");
-                    };
-            };
-            
-        });    
+}))
+//serialize user
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+//deserialize user  
+passport.deserializeUser(function(id, done) {
+    console.log("Deserializing User")
+    try {
+        User.findById(id).then(user=>{
+            done(null,user);
+        })
     }
     catch (err){
-        console.log(err.stack);
+        done(err);
     }
-});
-
-
-
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
   });
 
 
+//get routes
+app.get("/",function(req,res){
+    res.render("home")
+})
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+
+app.get("/register",function(req,res){
+    res.render("register")
+})
+
+app.get("/secrets",function(req,res){
+    if (req.isAuthenticated()){
+        res.render("secrets")
+    }
+    else {
+        res.redirect("/login")
+    }
+})
+
+app.get("/logout",function(req,res){
+    req.logout(function(err){
+        if(err){
+            console.log(err)
+        }
+        res.redirect("/");
+    });
+    
+})
+
+//post routes
+app.post("/register",function(req,res){
+    bcrypt.hash(req.body.password,10,function(err,hash){  //10 is SaltRounds
+        if (err){
+            console.log(err)
+        }
+        const user= new User ({
+            username:req.body.username,
+            password:hash
+        })
+        user.save()
+
+        passport.authenticate('local')(req,res,()=>{res.redirect("/secrets")}) 
+    })
+})   
+    
+
+
+app.post('/login', passport.authenticate('local', { successRedirect:"/secrets", failureRedirect: '/login' }));
+
+//listen
+app.listen(3000, ()=> {
+    console.log("Server Running on Port 3000")
+})
